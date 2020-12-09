23fd6ca94f8ad6f91f32de25cbefdb64c79fc914
Timothy Kerr <tmkkcc@gmail.com>
2020-12-09 15:28:41 -0600

    Seating chart init
    Initialize the seating chart with the prior seats that have been taken

---
+++ it-4320-final-proj/flask_wtforms_tutorial/resv_table.py
@@ -0,0 +1,16 @@
+#need to create a list containing 12 lists each of 4 items, all set to 0
+
+row0 =["O", "O", "O", "O"]
+row1 =["O", "O", "O", "O"]
+row2 =["O", "O", "O", "O"]
+row3 =["O", "O", "X", "O"]
+row4 =["O", "O", "O", "O"]
+row5 =["O", "O", "O", "O"]
+row6 =["O", "O", "O", "O"]
+row7 =["O", "O", "O", "O"]
+row8 =["O", "O", "O", "O"]
+row9 =["O", "O", "O", "O"]
+row10 =["O", "O", "O", "O"]
+row11 =["O", "O", "O", "O"]
+
+print(row3[2])
